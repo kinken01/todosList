45a96c6efd293fd02b8f75109668332456c69542
kinken01 <kinken01@gmail.com>
2023-09-26 23:07:54 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { attach } from "./store.js";
-import App from "./component/App.js"
+import { attach } from "../store.js";
+import App from "../component/App.js"
 
 attach(App, document.getElementById('root'))
